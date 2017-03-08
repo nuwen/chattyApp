5a33fea570b1dc72d3c256d6f23a06cf744277ab
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-08 18:44:34 +0000

    starting with server

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,6 @@ class App extends Component {
       }
       const messages = this.state.messages.concat(notification);
 
-
       this.setState({
           currentUser: {name: newName},
           messages: messages
